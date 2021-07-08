89c0a87abf81fa44934da31d0d460a84fbb4d434
junlinp <junlinp@qq.com>
2021-07-08 20:38:52 +0800

    Transform function programming

--- src/feature_matcher/exhaustive_pair_builder.cc
+++ src/feature_matcher/exhaustive_pair_builder.cc
@@ -6,7 +6,7 @@ void ExhaustivePairBuilder::BuildPair(SfMData& sfm_data, std::set<Pair>& pair_re
         return pair_item.first;
     };
 
-    std::vector<IndexT> index_vec = sfm_data.views | functor;
+    std::vector<IndexT> index_vec = sfm_data.views | Transform(functor) | ToVector();
 
     for (int i = 0;i < index_vec.size(); i++) {
         for (int j = i + 1; j < index_vec.size(); j++) {
--- src/internal/function_programming.hpp
+++ src/internal/function_programming.hpp
@@ -1,15 +1,101 @@
 #ifndef INTERNAL_FUNCTION_PROGRAMMING_H_
 #define INTERNAL_FUNCTION_PROGRAMMING_H_
-#include <vector>
 #include <algorithm>
-template<class Container, class Functor>
-auto operator|(Container&& container, Functor&& functor) {
-    using Container_Type = std::remove_reference_t<Container>;
-    std::vector<typename std::result_of_t<Functor(typename Container_Type::value_type)>> res;
+#include <vector>
 
-    for(const auto& item : container) {
-        res.push_back(functor(item));
+//
+//
+// std::vector<a> container1
+// functor : a -> b
+//
+// filter_functor : b -> bool
+// container1 | Transform(functor) | Filter(filter_functor) | ToVector();
+//
+//
+// So ContainerOutput operator|(Container1, TransformBuilder)
+// So ContainerOutput operator|(Container1, Output)
+//
+//
+template <class InputIterator, class Functor, class OutputType>
+struct TransformContainerIterator
+    : std::iterator<std::forward_iterator_tag, OutputType> {
+  TransformContainerIterator(InputIterator iterator, Functor functor)
+      : iterator_(iterator), functor_(functor){};
+
+  TransformContainerIterator operator++() {
+    TransformContainerIterator temp(iterator_, functor_);
+    ++iterator_;
+    return temp;
+  }
+
+  bool operator!=(const TransformContainerIterator& rhs) const {
+    return iterator_ != rhs.iterator_;
+  }
+
+  OutputType operator*() const { return functor_(*iterator_); }
+
+  InputIterator iterator_;
+  Functor functor_;
+};
+
+template <class InputContainer, class Functor>
+class TransformContainer {
+ public:
+  TransformContainer(InputContainer container, Functor functor)
+      : container_(std::forward<InputContainer>(container)),
+        functor_(std::forward<Functor>(functor)) {}
+  using InputType = typename std::decay_t<InputContainer>::value_type;
+  using OutputType = std::result_of_t<Functor(InputType)>;
+  using value_type = OutputType;
+  using iterator = TransformContainerIterator<typename std::decay_t<InputContainer>::iterator,
+                                      Functor, OutputType>;
+
+  iterator begin() { return iterator(container_.begin(), functor_); };
+  iterator end() { return iterator(container_.begin(), functor_); }
+
+ private:
+  InputContainer container_;
+  Functor functor_;
+};
+
+template <class Functor>
+class TransformContainerBuilder {
+ public:
+  TransformContainerBuilder(Functor functor)
+      : functor_(std::forward<Functor>(functor)) {}
+
+  template <class InputContainer>
+  auto Build(InputContainer&& container) {
+    return TransformContainer<InputContainer, Functor>(
+        std::forward<InputContainer>(container),
+        std::forward<Functor>(functor_));
+  }
+  Functor functor_;
+};
+
+template<class Functor>
+auto Transform(Functor&& functor) {
+    return TransformContainerBuilder<Functor>(std::forward<Functor>(functor));
+}
+
+template <class Container, class Functor>
+auto operator|(Container&& container, TransformContainerBuilder<Functor>&& builder) {
+    return builder.Build(container);
+}
+
+struct ToVectorBuilder{};
+
+ToVectorBuilder ToVector() {
+    return ToVectorBuilder();
+}
+template <class Container>
+auto operator|(Container&& container, ToVectorBuilder&& /**/) {
+    using Type = typename std::decay_t<Container>::value_type;
+    std::vector<Type> res;
+    for(auto item : container) {
+        res.emplace_back(item);
     }
     return res;
 }
+
 #endif  // INTERNAL_FUNCTION_PROGRAMMING_H_
