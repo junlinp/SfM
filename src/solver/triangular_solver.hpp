eeea50194ce8e2026aa62cebac233453123d7840
junlinp <junlinp@qq.com>
2021-11-21 20:12:28 +0800

    feat: Add DLTTriangular solver
    TODO: 6-order polynomial solver

--- src/solver/triangular_solver.hpp
+++ src/solver/triangular_solver.hpp
@@ -6,6 +6,7 @@
 #include "Eigen/Dense"
 
 #include "eigen_alias_types.hpp"
+using Mat33 = Eigen::Matrix<double, 3, 3>;
 
 void DLT(const EigenAlignedVector<Mat34>& p_matrixs,
 
@@ -15,4 +16,5 @@ void BundleAdjustmentTriangular(const EigenAlignedVector<Mat34>& p_matrixs,
                                 const EigenAlignedVector<Eigen::Vector3d>& obs,
                                 Eigen::Vector4d& X);
 
+void DLTTriangular(Eigen::Vector3d& lhs_obs, Eigen::Vector3d& rhs_obs, Mat34& lhs_p, Mat34& rhs_p, Eigen::Vector4d& X);
 #endif  // SRC_SOLVER_TRIANGULAR_SOLVER_HPP_
